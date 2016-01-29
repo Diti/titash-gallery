529310981a0639453c314766e064ab67576bf7c5
Diti Torterat <kra@diti.me>
2016-01-29 18:23:12 +0100

    Use dataset of field names

--- tests/workTest.php
+++ tests/workTest.php
@@ -11,24 +11,32 @@ class WorkTest extends PHPUnit_Framework_TestCase
         $this->work = new Work;
     }
 
-    public function testInit()
+    public function fieldNames()
     {
-        $work = get_class($this->work);
+        return array(
+            ['title'],
+            ['author'],
+            ['sequence'],
+            ['arc'],
+            ['date'],
+            ['category'],
+            ['tags'],
+            ['rating'],
+            ['medium'],
+            ['universe'],
+            ['characters'],
+            ['description'],
+            ['related'],
+            ['related_shop'],
+            ['copyright'],
+        );
+    }
 
-        $this->assertClassHasAttribute('title', $work);
-        $this->assertClassHasAttribute('author', $work);
-        $this->assertClassHasAttribute('sequence', $work);
-        $this->assertClassHasAttribute('arc', $work);
-        $this->assertClassHasAttribute('date', $work);
-        $this->assertClassHasAttribute('category', $work);
-        $this->assertClassHasAttribute('tags', $work);
-        $this->assertClassHasAttribute('rating', $work);
-        $this->assertClassHasAttribute('medium', $work);
-        $this->assertClassHasAttribute('universe', $work);
-        $this->assertClassHasAttribute('characters', $work);
-        $this->assertClassHasAttribute('description', $work);
-        $this->assertClassHasAttribute('related', $work);
-        $this->assertClassHasAttribute('related_shop', $work);
-        $this->assertClassHasAttribute('copyright', $work);
+    /**
+     * @dataProvider fieldNames
+     */
+    public function testClassHasField($field)
+    {
+        $this->assertClassHasAttribute($field, get_class($this->work));
     }
 }
